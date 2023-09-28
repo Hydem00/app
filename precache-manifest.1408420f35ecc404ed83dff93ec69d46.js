be8e7b36969e0124bda026bae4475a48d4e268e0
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2023-09-28 21:07:05 +0200

    Updates

---
+++ precache-manifest.1408420f35ecc404ed83dff93ec69d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b56e976ea6b045610907",
+    "url": "/static/css/main.f34f5363.chunk.css"
+  },
+  {
+    "revision": "b56e976ea6b045610907",
+    "url": "/static/js/main.fd13beb8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1565d96119828a9f2b76",
+    "url": "/static/js/2.db4a12da.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "9cc10d44d9ba6d04b6989da5747c19ea",
+    "url": "/index.html"
+  }
+];
